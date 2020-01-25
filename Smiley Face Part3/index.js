7081792f1c62adda32a8790cc7064281eb5f36e6
Alumanda Shakankale <alumandas.sil@gmail.com>
2020-01-25 14:52:06 +0200

    smiley face with react and ds

--- Smiley Face Part3/index.js
+++ Smiley Face Part3/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
+import {arc} from 'd3';
 
 const width = 960;
 const height = 500; 
@@ -9,12 +10,22 @@ const strokewidth = 30;
 const eyeOffsetX = 90;
 const eyeOffsetY = 100;
 const eyeRadius =  50;
+const mouthWidth = 20;
+const mouthRadius = 140;
+
+const mouthArc = arc()
+  .innerRadius(mouthRadius)
+  .outerRadius(mouthRadius + mouthWidth )
+  .startAngle(Math.PI / 2)
+  .endAngle(Math.PI * 3/2);
+
+
 
 const App = () => (
   <svg width = {width} height ={height}>
+    <g transform ={`translate(${centerX},${centerY})`}>
      <circle 
-             cx={centerX}
-             cy={centerY}
+            
              r={centerY - strokewidth/2}
              fill ="yellow"
              stroke ="black"
@@ -22,21 +33,22 @@ const App = () => (
        />
         
   <circle 
-             cx={centerX-eyeOffsetX}
-             cy={centerY-eyeOffsetY}
+             cx={-eyeOffsetX}
+             cy={-eyeOffsetY}
              r={eyeRadius}
              
     />
   
       <circle 
-             cx={centerX+eyeOffsetX}
-             cy={centerY-eyeOffsetY}
+             cx={eyeOffsetX}
+             cy={-eyeOffsetY}
              r={eyeRadius}
              
     />
     
-    
+    <path d={mouthArc()}/>
 
+    </g>
   </svg>
 );
 
